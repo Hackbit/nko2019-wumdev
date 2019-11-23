5b06ceaed51b24cfefb1cda6d6f6a8366276c2ee
Edqe <configxgt@gmail.com>
2019-11-23 07:56:56 +0700

    Move to Express :/

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-const Koa = require("koa");
-const Router = require("koa-router");
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
 
-const app = new Koa();
-const router = new Router();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-router.get('/', (ctx, next) => {
-    ctx.body = "Hello World";
-    next()
-});
+app.use(express.static('./public'));
 
-app.use(router.routes()).use(router.allowedMethods());
-app.listen(3000)
+app.listen(3000);
