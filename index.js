c74035a52ca3e883fc9cf009541ecb8b4da209d1
Edqe <configxgt@gmail.com>
2019-11-23 07:41:08 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = new Koa();
 const router = new Router();
 
 router.get('/', (ctx, next) => {
-    ctx.body = "Hello World"
+    ctx.body = "Hello World";
 });
 
 app.use(router.routes()).use(router.allowedMethods());
