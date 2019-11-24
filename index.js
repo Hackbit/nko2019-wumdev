50f865176f05a01d486ee4948dd6596058a8f6fd
Edqe <configxgt@gmail.com>
2019-11-24 12:17:56 +0700

    .

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const randExp = require('randexp');
-const WebSocket = require('ws');
 
 let server = app.listen(3000, () => {
     console.log("Server started");
@@ -44,24 +43,11 @@ io.on('connection', socket => {
             });
         });
 
-wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(message) {
-    let data = JSON.parse(message);
-    let event = data['e'];
-
-    if (event === 'new_connect') {
-      if (!Object.keys(users).includes(data['username'])) {
-        users[data['username']] = data['id'];
-        console.log('Registered user - %s', data['username']);
-        ws.send(JSON.stringify({e: 'USER_REGISTERED', data}));
-      }
-      else {
-        ws.send(JSON.stringify({e: 'ERROR', reason: 'USER_ALREADY_REGISTERED'}));
-      }
-    }
-  });
-
-  ws.send(JSON.stringify({e: 'USER_CONNECTED'}));
+        socket.on("disconnect", async () => {
+            await socket.broadcast.emit("userDisconnect", users[id].username);
+            delete users[id]
+        });
+    });
 });
 
 function genId() {
